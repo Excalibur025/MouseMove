99727a88be582f041dde0084b14f9806dec92a69
Excalibur025 <Excalibur025@gmail.com>
2020-07-02 18:14:03 -0500

    Add files via upload

---
+++ movemouse.ps1
@@ -0,0 +1,14 @@
+Clear-Host
+Echo "Moving mouse..."
+Add-Type -AssemblyName System.Windows.Forms
+
+$PlusOrMinus = 1
+while ($true)
+{
+  $p = [System.Windows.Forms.Cursor]::Position
+  $x = $p.X + $PlusOrMinus
+  $y = $p.Y + $PlusOrMinus
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
+  Start-Sleep -Seconds 30
+  $PlusOrMinus *= -1
+}
